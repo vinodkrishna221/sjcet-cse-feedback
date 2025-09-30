ee91ac3b3372c32f47aa9ebce82880a811b217fa
vinodkrishna <137382524+vinodkrishna221@users.noreply.github.com>
2025-09-30 08:38:57 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   build: {
-    outDir: 'build'
+    outDir: 'dist'
   },
   
   // Server configuration
